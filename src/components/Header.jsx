3355115eea5da919b65fd1e6286db4e20288ae3f
Denis Krestov <rupo@list.ru>
2024-01-15 23:08:08 +0300

    Create modules structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header () {
+   return (
+      <header className="header">
+            <img
+               className="App-logo"
+               src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/BMW_logo_%28gray%29.svg/2048px-BMW_logo_%28gray%29.svg.png"
+               alt="logo ipsum"
+            />
+         </header>
+   )
+}
+
+export default Header
