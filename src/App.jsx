58ccd787b33aa62004ec8874db968a61956171c5
Denis Krestov <rupo@list.ru>
2024-01-16 18:49:57 +0300

    Add more routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import "./App.css"
+import {Route, BrowserRouter, Switch} from "react-router-dom"
+
 import Header from "./components/Header/Header"
 import Navbar from "./components/Navbar/Navbar"
 import Profile from "./components/Profile/Profile"
 import Dialogs from "./components/Dialogs/Dialogs"
-import {Route, BrowserRouter, Switch} from "react-router-dom"
+import News from "./components/News/News"
+import Music from "./components/Music/Music"
+import Settings from "./components/Settings/Settings"
 
 function App() {
    return (
@@ -14,8 +18,11 @@ function App() {
             <Navbar />
             <div className="app-wrapper-content">
                <Switch>
-                  <Route path={"/dialogs"} component={Dialogs} />
                   <Route path={"/profile"} component={Profile} />
+                  <Route path={"/dialogs"} component={Dialogs} />
+                  <Route path={"/news"} component={News} />
+                  <Route path={"/music"} component={Music} />
+                  <Route path={"/settings"} component={Settings} />
                </Switch>
             </div>
          </div>
