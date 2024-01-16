45c01dba2e55f917e605f4134ee90f087a80f33a
Denis Krestov <rupo@list.ru>
2024-01-16 17:32:53 +0300

    Add first routing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,25 @@
 import React from "react"
-import s from './Navbar.module.css'
+import s from "./Navbar.module.css"
 
-function Navbar () {
+function Navbar() {
    return (
       <nav className={s.nav}>
-            <div className={s.item}>
-               <a href="">Profile</a>
-            </div>
-            <div className={s.item}>
-               Messages
-            </div>
-            <div className={`${s.item} ${s.active}`}>
-              News
-            </div>
-            <div className={s.item}>
-               <a href="">Music</a>
-            </div>
-            <div className={s.item}>
-               <a href="">Settings</a>
-            </div>
-         </nav>
+         <div className={s.item}>
+            <a href="/profile">Profile</a>
+         </div>
+         <div className={s.item}>
+            <a href="/dialogs">Messages</a>
+         </div>
+         <div className={`${s.item} ${s.active}`}>
+            <a href="/news">News</a>
+         </div>
+         <div className={s.item}>
+            <a href="/music">Music</a>
+         </div>
+         <div className={s.item}>
+            <a href="/settings">Settings</a>
+         </div>
+      </nav>
    )
 }
 
